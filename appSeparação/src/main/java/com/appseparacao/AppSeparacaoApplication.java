84091730a1224e68f8d0683edc337104e38040d4
Lucas Uvera <lucasuvera@gmail.com>
2026-02-15 16:04:33 -0300

    agora vai
    a

---
+++ appSeparação/src/main/java/com/appseparacao/AppSeparacaoApplication.java
@@ -0,0 +1,13 @@
+package com.appseparacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSeparacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSeparacaoApplication.class, args);
+	}
+
+}
---
+++ appSeparação/src/test/java/com/appseparacao/AppSeparacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appseparacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSeparacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
